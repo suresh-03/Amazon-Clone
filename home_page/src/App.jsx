fb403570de4f6a1344dad1db1c5840058b704540
suresh-03 <sureshkumar732003@gmail.com>
2023-08-07 21:03:41 +0530

    navbar template created

---
+++ home_page/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NavBar2 from './components/NavBar2'
+import NavBar from './components/NavBar'
+
+function App(){
+  return (
+    <>
+    <NavBar/>
+    <NavBar2/>
+    </>
+  );
+}
+export default App;
---
+++ home_page/src/components/NavBar2.jsx
@@ -0,0 +1,32 @@
+import '../styles/NavBar2.css'
+
+
+function NavBar2(){
+	return (
+		<div className="container">
+			<div className="nav-bar2">
+				<a href="/" className="nav-a-menu">
+					<span class="material-symbols-outlined">
+						menu
+					</span>
+					<span>All</span>
+				</a>
+				<a href="/" className="nav-a">Amazon miniTV</a>
+				<a href="/" className="nav-a">Sell</a>
+				<a href="/" className="nav-a">Best Sellers</a>
+				<a href="/" className="nav-a">Today's Deals</a>
+				<a href="/" className="nav-a">Mobiles</a>
+				<a href="/" className="nav-a">New Releases</a>
+				<a href="/" className="nav-a">Customer Service</a>
+				<a href="/" className="nav-a">Electronics</a>
+				<a href="/" className="nav-a">Home & Kitchen</a>
+				<a href="/" className="nav-a">Gift Ideas</a>
+				<a href="/" className="nav-a">Fashion</a>
+				<a href="/" className="nav-a">Amazon Pay</a>
+			</div>
+		</div>
+
+	);
+}
+
+export default NavBar2;
