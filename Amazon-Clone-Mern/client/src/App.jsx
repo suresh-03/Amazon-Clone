f2efe5b8fc48e8e80d3049b23ede6935e2b931e7
Suresh-03 <sureshkumar732003@gmail.com>
2023-10-19 06:38:58 +0530

    form component

--- Amazon-Clone-Mern/client/src/App.jsx
+++ Amazon-Clone-Mern/client/src/App.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import FormInput from "./components/FormInput";
 
 function App() {
-  return <div>App</div>;
+  const [value, setValue] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+    password: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`name:${value.name}\nemail:${value.email}`);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          type="text"
+          placeholder="Enter name"
+          value={value.name}
+          setValue={setValue}
+          name="name"
+        />
+        <FormInput
+          type="text"
+          value={value.mobile}
+          placeholder="Enter mobile number"
+          setValue={setValue}
+          name="mobile"
+        />
+        <FormInput
+          type="email"
+          value={value.email}
+          placeholder="Enter email"
+          setValue={setValue}
+          name="email"
+        />
+        <FormInput
+          type="password"
+          value={value.password}
+          placeholder="create password"
+          setValue={setValue}
+          name="password"
+        />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
