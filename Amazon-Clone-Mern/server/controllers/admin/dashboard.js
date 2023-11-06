2d63abd5c527a590ed11d18af8e0d4ba48aaac53
Suresh-03 <sureshkumar732003@gmail.com>
2023-11-06 05:58:22 +0530

    form scratch

--- Amazon-Clone-Mern/server/controllers/admin/dashboard.js
+++
@@ -1,9 +0,0 @@
-const Product = require("../../models/products");
-
-exports.dashboard = async (req, res) => {
-  const product = await Product.find();
-  res.json({
-    image:
-      product[0].categories[0].subCategories[0].brands[0].products[0].prodImage,
-  });
-};
